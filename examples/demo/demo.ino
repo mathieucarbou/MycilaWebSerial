459a0bc6f1bc79f6972aab7a8cc45d52d8692bab
Mathieu Carbou <mathieu.carbou@gmail.com>
2024-04-20 23:15:43 +0200

    Using PRIu32

--- examples/demo/demo.ino
+++ examples/demo/demo.ino
@@ -7,51 +7,50 @@
   Author: HomeboyC
 */
 #include <Arduino.h>
-#include <WiFi.h>
 #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
 #include <WebSerialLite.h>
+#include <WiFi.h>
 
 AsyncWebServer server(80);
 
-const char* ssid = ""; // Your WiFi SSID
+const char* ssid = "";     // Your WiFi SSID
 const char* password = ""; // Your WiFi Password
 
-
 /* Message callback of WebSerial */
-void recvMsg(AsyncWebSocketClient* client, const String& msg){
+void recvMsg(AsyncWebSocketClient* client, const String& msg) {
   WebSerial.println("Received Data...");
   WebSerial.println(msg);
 }
 
 void setup() {
-    Serial.begin(9600);
-    WiFi.mode(WIFI_STA);
-    WiFi.begin(ssid, password);
-    if (WiFi.waitForConnectResult() != WL_CONNECTED) {
-        Serial.printf("WiFi Failed!\n");
-        return;
-    }
-    Serial.print("IP Address: ");
-    Serial.println(WiFi.localIP());
-    // WebSerial is accessible at "<IP Address>/webserial" in browser
-    WebSerial.begin(&server);
-    /* Attach Message Callback */
-    WebSerial.onMessage(recvMsg);
-    server.begin();
+  Serial.begin(9600);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  if (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.printf("WiFi Failed!\n");
+    return;
+  }
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+  // WebSerial is accessible at "<IP Address>/webserial" in browser
+  WebSerial.begin(&server);
+  /* Attach Message Callback */
+  WebSerial.onMessage(recvMsg);
+  server.begin();
 }
 
 void loop() {
-    delay(2000);
+  delay(2000);
 
-    // we suggest you to use `print + "\n"` instead of `println`
-    // because the println will send "\n" separately, which means
-    // it will cost a sending buffer just for storage "\n". (there
-    // only 8 buffer space in ESPAsyncWebServer by default)
-    WebSerial.print(F("IP address: "));
-    // if not use toString the ip will be sent in 7 part,
-    // which means it will cost 7 sending buffer.
-    WebSerial.println(WiFi.localIP().toString());
-    WebSerial.printf("Millis=%lu\n", millis());
-    WebSerial.printf("Free heap=[%u]\n", ESP.getFreeHeap());
+  // we suggest you to use `print + "\n"` instead of `println`
+  // because the println will send "\n" separately, which means
+  // it will cost a sending buffer just for storage "\n". (there
+  // only 8 buffer space in ESPAsyncWebServer by default)
+  WebSerial.print(F("IP address: "));
+  // if not use toString the ip will be sent in 7 part,
+  // which means it will cost 7 sending buffer.
+  WebSerial.println(WiFi.localIP().toString());
+  WebSerial.printf("Millis=%lu\n", millis());
+  WebSerial.printf("Free heap=[%" PRIu32 "]\n", ESP.getFreeHeap());
 }
--- examples/demo_ap/demo_ap.ino
+++ examples/demo_ap/demo_ap.ino
@@ -7,52 +7,48 @@
   Author: HomeboyC
 */
 #include <Arduino.h>
-#include <WiFi.h>
 #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
 #include <WebSerialLite.h>
+#include <WiFi.h>
+#include <inttypes.h>
 
 AsyncWebServer server(80);
 
-const char* ssid = ""; // Your WiFi AP SSID 
+const char* ssid = "";     // Your WiFi AP SSID
 const char* password = ""; // Your WiFi Password
 
-
 /* Message callback of WebSerial */
-void recvMsg(AsyncWebSocketClient* client, const String& msg){
+void recvMsg(AsyncWebSocketClient* client, const String& msg) {
   WebSerial.println("Received Data...");
   WebSerial.println(msg);
 }
 
 void setup() {
-    Serial.begin(9600);
-    WiFi.softAP(ssid, password);
+  Serial.begin(9600);
+  WiFi.softAP(ssid, password);
 
-    IPAddress IP = WiFi.softAPIP();
-    Serial.print("AP IP address: ");
-    Serial.println(IP);
-    // WebSerial is accessible at "<IP Address>/webserial" in browser
-    WebSerial.begin(&server);
-    /* Attach Message Callback */
-    WebSerial.onMessage(recvMsg);
-    server.begin();
+  IPAddress IP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(IP);
+  // WebSerial is accessible at "<IP Address>/webserial" in browser
+  WebSerial.begin(&server);
+  /* Attach Message Callback */
+  WebSerial.onMessage(recvMsg);
+  server.begin();
 }
 
 void loop() {
-    delay(2000);
+  delay(2000);
 
-    // we suggest you to use `print + "\n"` instead of `println`
-    // because the println will send "\n" separately, which means
-    // it will cost a sending buffer just for storage "\n". (there
-    // only 8 buffer space in ESPAsyncWebServer by default)
-    WebSerial.print(F("IP address: "));
-    // if not use toString the ip will be sent in 7 part,
-    // which means it will cost 7 sending buffer.
-    WebSerial.println(WiFi.localIP().toString());
-    WebSerial.printf("Millis=%lu\n", millis());
-#if ESP_IDF_VERSION_MAJOR < 5
-    WebSerial.printf("Free heap=[%u]\n", ESP.getFreeHeap());
-#else
-  WebSerial.printf("Free heap=[%lu]\n", ESP.getFreeHeap());
-#endif
+  // we suggest you to use `print + "\n"` instead of `println`
+  // because the println will send "\n" separately, which means
+  // it will cost a sending buffer just for storage "\n". (there
+  // only 8 buffer space in ESPAsyncWebServer by default)
+  WebSerial.print(F("IP address: "));
+  // if not use toString the ip will be sent in 7 part,
+  // which means it will cost 7 sending buffer.
+  WebSerial.println(WiFi.localIP().toString());
+  WebSerial.printf("Millis=%lu\n", millis());
+  WebSerial.printf("Free heap=[%" PRIu32 "]\n", ESP.getFreeHeap());
 }
